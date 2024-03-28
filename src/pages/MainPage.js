37a05d59c3a3b09aec4a0970dbb34fc38aa4edb7
Desswell <46073080+desswell@users.noreply.github.com>
2024-03-28 15:15:36 +0300

    logo added

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -7,6 +7,7 @@ import SelectMulti from "../components/select_checkbox";
 import Button from "../components/Button";
 import axios from 'axios';
 import {DotLoader} from "react-spinners";
+import {ReactComponent as ReactLogo} from '../svgs/BAUM_AI_P-05.svg'
 
 export function MainPage() {
     const [loader, setLoader] = useState(false)
@@ -52,70 +53,73 @@ export function MainPage() {
     }
 
     return(
-        <div className="main-page-container">
-            {data && <div className='graph'>
-                <Plot
-                    data={data.data}
-                    layout={data.layout}
-                />
-            </div>}
-            <div>
-                <div className="input-div">
-                    <SelectMulti options={[
-                        {key: 'System', cat: 'Array'},
-                        {key: 'StoragePool001', cat: 'StoragePool1'},
-                        {key: 'StoragePool002', cat: 'StoragePool2'},
-                    ]} handleChange={handleChangeParam} title='Параметры' state={[{key: 'System', cat: 'Array'}]}/>
-                </div>
-                <div className="input-div">
-                    <StyledInput children='Признаки' value={sigh} onChange={(event) => setSigh(event.target.value)}/>
-                </div>
-                <div className="input-div">
-                    <StyledInput children='Целевые признаки' value={sighParam}
-                                 onChange={(event) => setSighParam(event.target.value)}/>
-                </div>
-                <div className="input-div">
-                    <Select children={[
-                        {value: 'auto_interval', label: '1. Автоматический'},
-                        {value: 'advanced_interval', label: '2. Ручной (продвинутый)'},
-                    ]} title='Режим выбора окна' value={window} onChange={(event) => setWindow(event.target.value)}/>
-                </div>
-                {window==='advanced_interval' && <div>
+        <div>
+            <ReactLogo style={{ width: '200px', height: 'auto', marginLeft: '30px'}}/>
+            <div className="main-page-container">
+                {data && <div className='graph'>
+                    <Plot
+                        data={data.data}
+                        layout={data.layout}
+                    />
+                </div>}
+                <div>
+                    <div className="input-div">
+                        <SelectMulti options={[
+                            {key: 'System', cat: 'Array'},
+                            {key: 'StoragePool001', cat: 'StoragePool1'},
+                            {key: 'StoragePool002', cat: 'StoragePool2'},
+                        ]} handleChange={handleChangeParam} title='Параметры' state={[{key: 'System', cat: 'Array'}]}/>
+                    </div>
+                    <div className="input-div">
+                        <StyledInput children='Признаки' value={sigh} onChange={(event) => setSigh(event.target.value)}/>
+                    </div>
+                    <div className="input-div">
+                        <StyledInput children='Целевые признаки' value={sighParam}
+                                     onChange={(event) => setSighParam(event.target.value)}/>
+                    </div>
                     <div className="input-div">
                         <Select children={[
-                            {value: 'День', label: 'День'},
-                            {value: 'Неделя', label: 'Неделя'},
-                            {value: 'Месяц', label: 'Месяц'},
-                            {value: 'Год', label: 'Год'},
-                        ]} title='Интервал' value={interval} onChange={(event) => setInterval(event.target.value)}/>
+                            {value: 'auto_interval', label: '1. Автоматический'},
+                            {value: 'advanced_interval', label: '2. Ручной (продвинутый)'},
+                        ]} title='Режим выбора окна' value={window} onChange={(event) => setWindow(event.target.value)}/>
+                    </div>
+                    {window==='advanced_interval' && <div>
+                        <div className="input-div">
+                            <Select children={[
+                                {value: 'День', label: 'День'},
+                                {value: 'Неделя', label: 'Неделя'},
+                                {value: 'Месяц', label: 'Месяц'},
+                                {value: 'Год', label: 'Год'},
+                            ]} title='Интервал' value={interval} onChange={(event) => setInterval(event.target.value)}/>
+                        </div>
+                        <div className="input-div">
+                            <StyledInput children='Количество интервалов' value={intervalNum} onChange={(event) => setIntervalNum(event.target.value)}/>
+                        </div>
+                    </div>
+                    }
+                    <div className="input-div">
+                        <SelectMulti options={[
+                            {key: 'LEVEL0', cat: 'Уровень 1'},
+                            {key: 'LEVEL1', cat: 'Уровень 2'},
+                            {key: 'LEVEL2', cat: 'Уровень 3'},
+                        ]} handleChange={handleChangeLevel} title='Предсказание для' state={[{key: 'LEVEL0', cat: 'Уровень 1'}]}/>
                     </div>
                     <div className="input-div">
-                        <StyledInput children='Количество интервалов' value={intervalNum} onChange={(event) => setIntervalNum(event.target.value)}/>
+                        <Checkbox title='Использовать для прогноза StoragePool' type='checkbox'
+                                  value={storagePool} onChange={(event) => setStoragePool(event.target.value)}/>
                     </div>
-                </div>
-                }
-                <div className="input-div">
-                    <SelectMulti options={[
-                        {key: 'LEVEL0', cat: 'Уровень 1'},
-                        {key: 'LEVEL1', cat: 'Уровень 2'},
-                        {key: 'LEVEL2', cat: 'Уровень 3'},
-                    ]} handleChange={handleChangeLevel} title='Предсказание для' state={[{key: 'LEVEL0', cat: 'Уровень 1'}]}/>
-                </div>
-                <div className="input-div">
-                    <Checkbox title='Использовать для прогноза StoragePool' type='checkbox'
-                              value={storagePool} onChange={(event) => setStoragePool(event.target.value)}/>
-                </div>
-                {window==='auto_interval' && <div className="input-div">
-                    <Checkbox title='find_global' type='checkbox'
-                              value={global} onChange={(event) => setGlobal(event.target.value)}/>
-                </div>}
-                <div className="input-div">
-                    <Checkbox title='Облако точек' type='checkbox'
-                              value={cloud} onChange={(event) => setCloud(event.target.value)}/>
-                </div>
-                <div className="input-div">
-                    {!loader && <Button children='Визуализировать' onClick={handleClick}/>}
-                    {loader && <DotLoader color='#5558FA'/>}
+                    {window==='auto_interval' && <div className="input-div">
+                        <Checkbox title='find_global' type='checkbox'
+                                  value={global} onChange={(event) => setGlobal(event.target.value)}/>
+                    </div>}
+                    <div className="input-div">
+                        <Checkbox title='Облако точек' type='checkbox'
+                                  value={cloud} onChange={(event) => setCloud(event.target.value)}/>
+                    </div>
+                    <div className="input-div">
+                        {!loader && <Button children='Визуализировать' onClick={handleClick}/>}
+                        {loader && <DotLoader color='#5558FA'/>}
+                    </div>
                 </div>
             </div>
         </div>
